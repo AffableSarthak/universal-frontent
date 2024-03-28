080250c8b24b4e015d8f89f80eb7504e70ecd767
sarthak <sarthaksharmaanuj@gmail.com>
2024-03-28 14:20:35 +0530

    Fix event bubbling issue

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,6 +1,13 @@
 "use client";
 
-import { useReducer, useState } from "react";
+import {
+  FormEvent,
+  SyntheticEvent,
+  useEffect,
+  useReducer,
+  useRef,
+  useState,
+} from "react";
 import { productReducer } from "./productReducer";
 import { Product, ProductCategory } from "./types";
 
@@ -10,46 +17,18 @@ export default function ProductHome() {
     initialProductInventory
   );
 
-  const defaultFormState = {
-    id: "",
-    name: "",
-    category: ProductCategory.el,
-    quantity: 0,
-  };
-  const [formValue, setFormValue] = useState<Product>(defaultFormState);
-
-  const handleFormChange = (e: any) => {
-    const { name, value } = e.target;
-    console.log(name, value);
-    setFormValue({
-      ...formValue,
-      [name]: value,
-    });
-  };
-
-  const handleProductAddition = (e: any) => {
-    e.preventDefault();
-    console.log(formValue);
-    const newProduct: Product = {
-      ...formValue,
-      id: crypto.randomUUID(),
-    };
+  const handleProductAddition = (formValue: Product) => {
     dispatch({
       type: "ADD",
-      pi: newProduct,
+      pi: formValue,
     });
-    setFormValue(defaultFormState);
   };
 
   return (
     <article className="h-screen w-screen bg-slate-200">
       <div className="flex justify-evenly items-center">
         <div>
-          <ProductForm
-            handleFormChange={handleFormChange}
-            formValue={formValue}
-            handleProductAddition={handleProductAddition}
-          />
+          <ProductForm handleProductAddition={handleProductAddition} />
         </div>
         <div className="overflow-y-auto max-h-96">
           <DisplayProducts products={products} />
@@ -61,13 +40,46 @@ export default function ProductHome() {
 
 const ProductForm = ({
   handleProductAddition,
-  formValue,
-  handleFormChange,
-}: any) => {
+}: {
+  handleProductAddition: (formValue: Product) => void;
+}) => {
+  const defaultFormState = {
+    id: "",
+    name: "",
+    category: ProductCategory.el,
+    quantity: 0,
+  };
+  const [formValue, setFormValue] = useState<Product>(defaultFormState);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setFormValue({
+      ...formValue,
+      quantity: count,
+    });
+  }, [count]);
+
+  const handleFormChange = (e: any) => {
+    const { name, value } = e.target;
+    console.log(name, value);
+    setFormValue({
+      ...formValue,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // TODO handle errors and feedbacks
+    // USE zod
+    handleProductAddition({ ...formValue, id: crypto.randomUUID() });
+  };
+
   return (
     <form
       className="flex flex-col min-h-screen justify-center items-center gap-4 text-black"
-      onSubmit={handleProductAddition}
+      onSubmit={(e) => handleFormSubmit(e)}
     >
       <label>
         Product Name:
@@ -95,36 +107,37 @@ const ProductForm = ({
 
       <label>
         Add your quantity:
-        <div className="flex gap-2">
-          {/* <button
-        className="bg-green-200 text-black text-md px-3 font-bold"
-        onClick={handleFormChange}
-      >
-        -
-      </button> */}
-          <div>
-            <input
-              type="number"
-              name="quantity"
-              id="quantity"
-              value={formValue.quantity}
-              onChange={handleFormChange}
-            />
-          </div>
-          {/* <div>
-        <button
-          className="bg-green-200 text-black text-md px-3 font-bold"
-          onClick={handleFormChange}
+        {/* <button
+          className="text-sm bg-green-400 rounded-lg px-2 m-2"
+          disabled={count === 0}
+          onClick={() => setCount((c) => c - 1)}
         >
-          +
+          delete count
         </button>
-      </div> */}
-        </div>
+        <button
+          className="text-sm bg-green-400 rounded-lg px-2 m-2"
+          onClick={(e) => {
+            e.stopPropagation();
+            setCount(count + 1);
+          }}
+        >
+          Add count
+        </button> */}
+        <input
+          type="number"
+          name="quantity"
+          id="quantity"
+          value={formValue.quantity}
+          onChange={handleFormChange}
+          // disabled
+          className="text-right w-full px-3 rounded-md m-2"
+        />
       </label>
 
       <button
         type="submit"
-        className="bg-green-200 text-black text-md px-3 font-bold"
+        className="bg-green-200 text-black text-md px-3 font-bold disabled:opacity-50"
+        disabled={formValue.name === "" || formValue.quantity === 0}
       >
         ADD
       </button>
@@ -133,6 +146,18 @@ const ProductForm = ({
 };
 
 const DisplayProducts = ({ products }: { products: Product[] }) => {
+  if (products.length === 0) {
+    return (
+      <>
+        <div className="text-xl font-semibold text-black">
+          Inventory is currently empty
+        </div>
+        <div className="text-xs text-center text-green-800">
+          Add from the form in the left side{" "}
+        </div>
+      </>
+    );
+  }
   return (
     <>
       {products?.map((p) => (
@@ -144,6 +169,8 @@ const DisplayProducts = ({ products }: { products: Product[] }) => {
           <div>{p.name}</div>
           <div>{p.category}</div>
           <div>{p.quantity}</div>
+          {/* <button>delete</button>
+          <button>edit</button> */}
         </div>
       ))}
     </>
